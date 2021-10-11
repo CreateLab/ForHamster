a1f0cf09e9cb25f0b7f7884e4a5917d76dd23e12
lary <f98f96@gmail.com>
2021-10-11 16:11:36 +0300

    Initial commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        Parent i= new Child() ;
+        Test test = new Test();
+        test.DoSmth(i);
+    }
+}
---
+++ src/main/java/Parent.java
@@ -0,0 +1,6 @@
+public class Parent implements Interface{
+    @Override
+    public void getMyName() {
+        System.out.println("Im parent");
+    }
+}
